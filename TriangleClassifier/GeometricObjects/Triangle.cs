b3c0d3c24b948f4ddcdee5294f17fd6667af79f1
bvesperman <bvesperman@gmail.com>
2017-10-04 14:25:41 -0500

    added triangle type enum and more tests

--- TriangleClassifier/GeometricObjects/Triangle.cs
+++ TriangleClassifier/GeometricObjects/Triangle.cs
@@ -8,6 +8,7 @@ namespace GeometricObjects
 {
     public class Triangle: Shape
     {
+        private TriangleTypes _type;
         protected List<double> _angles = new List<double>();
 
         public Triangle(double sideA, double sideB, double sideC)
@@ -15,21 +16,47 @@ namespace GeometricObjects
             _sides.Add(sideA);
             _sides.Add(sideB);
             _sides.Add(sideC);
+
+            CalculateAngles();
         }
 
         public void CalculateAngles()
         {
             _angles.Add(solveAngleFromSides(_sides[0], _sides[1], _sides[2]));
+            _angles.Add(solveAngleFromSides(_sides[1], _sides[2], _sides[0]));
+            _angles.Add(solveAngleFromSides(_sides[2], _sides[0], _sides[1]));
         }
 
+        public TriangleTypes Type
+        {
+            get { return _type; }
+        }
+        /// <summary>
+        /// calculates the angle of the triangle that is between sides b and c
+        /// </summary>
+        /// <param name="sideA"></param>
+        /// <param name="sideB"></param>
+        /// <param name="sideC"></param>
+        /// <returns>the Angle in degrees</returns>
         private double solveAngleFromSides(double sideA, double sideB, double sideC)
         {
             double numerator = Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - Math.Pow(sideA, 2);
             double denominator = 2 * sideB * sideC;
 
-            double angle = Math.Acos(numerator/ denominator);
+            double angle = Math.Acos(numerator/ denominator) * (180 / Math.PI);
 
             return angle;
+        }
+
+        public  bool IsValidTriangle()
+        {
+            double total = 0.0;
+            foreach(double angle in _angles)
+            {
+                total += angle;
+            }
+
+            return (total < 180.0000001 && total > 179.99999);
         }
     }
 }
---
+++ TriangleClassifier/Tests/TriangleTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometricObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void TriangleTest()
+        {
+            Triangle myTri = new Triangle(1.5, 1.5, 1.5);
+            Assert.IsTrue(myTri.IsValidTriangle());
+            myTri = new Triangle(3, 4, 4);
+            Assert.IsTrue(myTri.IsValidTriangle());
+            myTri = new Triangle(4.4, 7.3, 9.1);
+            Assert.IsTrue(myTri.IsValidTriangle());
+            myTri = new Triangle(4, 6, 11);
+            Assert.IsFalse(myTri.IsValidTriangle());
+        }
+    }
+}
